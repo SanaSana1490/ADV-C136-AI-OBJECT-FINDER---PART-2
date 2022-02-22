6417575a86bce815e72b07ad7836b6c181ea6a8d
SanaSana1490 <100184058+SanaSana1490@users.noreply.github.com>
2022-02-22 11:05:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+Name = "";
+video = "";
+Status = "";
+
+function setup(){
+    canvas = createCanvas(380, 280);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 280);
+    video.hide();
+}
+
+function start(){
+        objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+        document.getElementById("status").innerHTML = "Status: Detecting Objects";
+        document.getElementById("name").value = Name;
+    }
+
+    function modelLoaded(){
+        console.log("Model Loaded!");
+        Status = true;
+    }
+
+    function draw(){
+        image(video, 0, 0, 380, 280);
+    }
